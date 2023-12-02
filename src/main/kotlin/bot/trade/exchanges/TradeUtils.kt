135860518b3b870aef2f2ed417e9c1cb6ab9113a
petrovMA <petrovma92@gmail.com>
2023-12-03 01:21:28 +0800

    added `initKlineForIndicator` function

--- src/main/kotlin/bot/trade/exchanges/TradeUtils.kt
+++ src/main/kotlin/bot/trade/exchanges/TradeUtils.kt
@@ -2,10 +2,40 @@ package bot.trade.exchanges
 
 import bot.trade.libs.ListLimit
 import bot.trade.exchanges.clients.*
+import bot.trade.exchanges.libs.KlineConverter
+import bot.trade.libs.m
 import bot.trade.libs.readConf
 import mu.KLogger
 import java.math.BigDecimal
+import java.time.Duration
 
+
+fun initKlineForIndicator(client: ClientByBit, pair: TradePair, outputKlineInterval: Duration /* todo Add functionality: one call `getCandlestickBars` for generate several KlineConverters */, klineAmount: Int): KlineConverter {
+    val inputKlineInterval = 5.m() to INTERVAL.FIVE_MINUTES
+
+    val milliseconds = outputKlineInterval.toMillis() * klineAmount
+
+    val klineConverter = KlineConverter(inputKlineInterval.first, outputKlineInterval, klineAmount)
+
+    val endTime = System.currentTimeMillis().let { it - it % inputKlineInterval.first.toMillis() }
+    var startTime = endTime - milliseconds
+
+    do {
+        val fiveMinutes = client.getCandlestickBars(
+            pair = pair,
+            interval = inputKlineInterval.second,
+            countCandles = 1000,
+            start = startTime,
+            end = null
+        )
+
+        klineConverter.addCandlesticks(*fiveMinutes.toTypedArray())
+        startTime = fiveMinutes.first().closeTime
+
+    } while (startTime < endTime)
+
+    return klineConverter
+}
 
 fun calcAveragePriceStatic(
     currentCandlestickList: ListLimit<Candlestick>,
